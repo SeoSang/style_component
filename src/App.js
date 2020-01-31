a69685e2f632e0b2d8b1b710003ab2f4db88e637
SeoSang <ddrrpg@naver.com>
2020-01-31 15:17:26 +0900

    style component 적용, createGlobalStyle, as 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Fragment } from "react"
-import "./App.css"
+import styled, { createGlobalStyle } from "styled-components"
+
+const GloabalStyle = createGlobalStyle`
+body {
+  margin:0;
+  padding:0;
+  background-color:blue;
+}
+`
+
 function App() {
   return (
     <Fragment>
-      <button className="button">Hello</button>
-      <button className="button button--success">Hello</button>
-      <button className="button button--danger">Hello</button>
+      <Container>
+        <Button>test</Button>
+        <Button danger>Danger </Button>
+        <Button as="a" href="http://google.com" css="{text-decoration: none}">
+          Google
+        </Button>
+      </Container>
+      <GloabalStyle />
     </Fragment>
   )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #ff7979;
+`
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  font-weight: 600;
+  -webkit-appearance: none;
+  cursor: pointer;
+  background-color: #6ab04c;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#eb4d4b" : "white")};
+`
 
 export default App
